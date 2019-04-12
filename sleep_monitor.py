60b21d9e939940bb2c2edec0501b2c81e717e019
apoorvam <app.apoorva@gmail.com>
2019-04-12 11:25:45 -0400

    [WIP] FFT on filtered data

--- sleep_monitor.py
+++ sleep_monitor.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import style 
+from matplotlib import style
+import scipy.fftpack
+import scipy as sp
 
-input_file_path = "HMP_Dataset/Liedown_bed/Accelerometer-2011-03-29-09-19-22-liedown_bed-f1.txt"
+input_file_path = "HMP_Dataset/Liedown_bed/Accelerometer-2011-06-02-17-21-57-liedown_bed-m1.txt"
 sampling_frequency = 32
 segment_window_size = 1
 acceleration_threshold = 10
@@ -13,6 +15,7 @@ style.use('ggplot')
 
 def segment(data):
   size = len(data)
+  print('Segmenting data...')
   
   # CONVERT THE ACCELEROMETER DATA INTO REAL ACCELERATION VALUES
   # mapping from [0. .63] to[-14.709.. + 14.709]
@@ -23,9 +26,12 @@ def segment(data):
   print("Number of segments:", len(segmented_data))
   print("Size of each segment:", len(segmented_data[0]))
 
-  filtered_data = list(filter(lambda segment: is_valid_segment(segment), segmented_data))
+  print('Removing segments with motion...')
+  filtered_data = np.array(list(filter(lambda segment: is_valid_segment(segment), segmented_data)))
   print("Number of filtered segments:", len(filtered_data))
-
+  flattened_data = np.array([e for sl in filtered_data for e in sl])
+  print("Number of records:", len(flattened_data))
+  return flattened_data
 
 def is_valid_segment(segment):
   window_size = len(segment)
@@ -42,14 +48,14 @@ def plot_ax(data):
   plt.xlabel('TIME (s)')
   plt.draw()
   plt.pause(5)
+  plt.close()
 
 def preprocess(data):
-  print(data)
+  print('Denoisifying data...')
   data[:,0] = denoisify(data[:,0], tv_filter_lambda, len(data))
   data[:,1] = denoisify(data[:,1], tv_filter_lambda, len(data))
   data[:,2] = denoisify(data[:,2], tv_filter_lambda, len(data))
-  print(data)
-  return
+  return data
 
 def clip(x, b):
   return np.where(x <= b, x, b * np.sign(x))
@@ -65,13 +71,37 @@ def denoisify(y, lambda_value, nit):
     x = y - np.c_[-z[0][0], -np.diff(z, n=1), z[len(z)-1][len(z)-1]]
     abs = np.abs(x-y)
     J[0][k] = np.sum(abs*abs) + lambda_value * np.sum(np.abs(np.diff(x)))
-    z = z + (1/alpha) * np.diff(x) # z + 1/alpha D z
+    z = z + (1/alpha) * np.diff(x)
     z = np.vectorize(clip)(z, T)
   return x
 
+# Fast fourier transform
+def apply_fft(data):
+  print('Converting time domain signal to frequency domain...')
+  x = data[:,0]
+
+  X = sp.fftpack.fft(x)
+  freqs = sp.fftpack.fftfreq(len(x)) * sampling_frequency
+  # positive_freqs = np.array(list(map(lambda f: 0 if f < 0 or f > 0.5 else f, freqs)))
+
+  fig, ax = plt.subplots()
+  ax.stem(freqs, np.abs(X))
+
+  ax.set_xlabel('Frequency in Hertz [Hz]')
+  ax.set_ylabel('Magnitude')
+  ax.set_title('FFT of the filtered data')
+  ax.set_xlim(-0.25, 1)
+  print(max(abs(X)))
+  print(max(abs(freqs)))
+  plt.show()
+  return
+
 if __name__ == '__main__':
   data = np.loadtxt(input_file_path)
+  print("Number of records:", len(data))
+
   plot_ax(data)
-  segment(data)
-  preprocess(data)
+  data = segment(data)
+  data = preprocess(data)
   plot_ax(data)
+  apply_fft(data)
