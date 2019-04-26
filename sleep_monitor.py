ac0e6777a07a9ff1546810922275a3feff305e29
apoorvam <app.apoorva@gmail.com>
2019-04-26 13:51:44 -0400

    Apply kalman filter for multi axis fusion in sleep monitor

--- sleep_monitor.py
+++ sleep_monitor.py
@@ -4,6 +4,7 @@ from matplotlib import style
 import scipy.fftpack
 import scipy as sp
 from scipy import signal
+import math
 import warnings
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 
@@ -79,8 +80,52 @@ def denoisify(y, lambda_value, nit):
     z = np.vectorize(clip)(z, T)
   return x
 
+def apply_kalman_filter(data):
+  print("Performing multi-axis fusion by Kalman filter...")
+  segment_size_for_kalman = 4 * sampling_frequency
+  n_segments = math.ceil(len(data)/segment_size_for_kalman) +1
+  rhat = np.zeros(n_segments)    # a posteri estimate of rr
+  rhatminus = np.zeros(n_segments) # a priori estimate of rr
+  p = np.zeros(n_segments)  # a posteri error estimate
+  pminus = np.zeros(n_segments)  # a priori error estimate
+  k = np.zeros(n_segments) # kalman gain
+
+  r_measurement = np.zeros((n_segments, 3)) # observation measurements
+
+  # initial guesses
+  p[0] = 1.0  
+  rhat[0] = 40
+  for segment_number in range(1, n_segments):
+    segment = get_segment(data, segment_number-1, segment_size_for_kalman)
+    f_x, fft_data_x, max_amp_x, max_index_x = fft(segment[:,0])
+    f_y, fft_data_y, max_amp_y, max_index_y = fft(segment[:,1])
+    f_z, fft_data_z, max_amp_z, max_index_z = fft(segment[:,2])
+    r_measurement[segment_number] = [f_x[max_index_x] * 60, f_y[max_index_y] * 60, f_z[max_index_z] * 60]  
+    variance_x = np.var(r_measurement[:,0])
+    variance_y = np.var(r_measurement[:,1])
+    variance_z = np.var(r_measurement[:,2])
+    total_variance = variance_x + variance_y + variance_z
+
+    # time update
+    rhatminus[segment_number] = rhat[segment_number-1]
+    pminus[segment_number] = p[segment_number-1]
+
+    # measurement update
+    n_f = [variance_x/total_variance, variance_y/total_variance, variance_z/total_variance]
+    z = (n_f[0] * r_measurement[segment_number][0]) + (n_f[1] * r_measurement[segment_number][1]) + (n_f[2] * r_measurement[segment_number][2])
+    r = np.sqrt(np.sum(list(map(lambda index: np.square(n_f[index] * (r_measurement[segment_number][index] - z)), [0, 1, 2])))) # variance of the measurement noise
+    if (pminus[segment_number]!=0 or r != 0):
+      k[segment_number] = pminus[segment_number] /(pminus[segment_number] + r)
+
+    rhat[segment_number] = rhatminus[segment_number] + k[segment_number] * (z - rhatminus[segment_number]) 
+    p[segment_number] = (1 - k[segment_number]) * pminus[segment_number]
+
+  print("Breathing rate from Kalman filter:", rhat[n_segments-1])
+
+def get_segment(data, segment_number, segment_size):
+  return data[segment_number*segment_size: segment_number*segment_size+segment_size]
+
 def apply_fft_on_xyz(data):
-  print('Converting time domain signal to frequency domain by FFT...')
   print('X-Axis')
   r_x = apply_fft(data[:,0], 'FFT of the filtered data X-Axis')
   print('Y-Axis')
@@ -88,17 +133,13 @@ def apply_fft_on_xyz(data):
   print('Z-Axis')
   r_z = apply_fft(data[:,2], 'FFT of the filtered data Z-Axis')
   print('Average Respiratory rate (bpm):', (r_x+r_y+r_z)/3)
+  return r_x, r_y, r_z
 
-# Fast fourier transform
-def apply_fft(data, title):
-  acc_data = data
-  acc_data = sp.signal.detrend(acc_data)
-  N = len(acc_data)
-
-  fft_data = sp.fftpack.fft(acc_data)
+def fft(data):
+  N = len(data)
+  data = sp.signal.detrend(data)
+  fft_data = sp.fftpack.fft(data)
   f = np.linspace(0, sampling_frequency, N)
-
-  plt.plot(f[:N // 2], np.abs(fft_data)[:N // 2] * 1 / N)
 
   max_amp = 0
   max_index = 0
@@ -111,11 +152,18 @@ def apply_fft(data, title):
       max_amp = amp
       max_index = index
     index = index+1
+  return f, fft_data, max_amp, max_index
+
+# Fast fourier transform
+def apply_fft(acc_data, title):
+  N = len(acc_data)
+  f, fft_data, max_amp, max_index = fft(acc_data)
 
   print('Max Amplitude:', max_amp)
   print('Respiratory rate:',f[max_index])
   print('Respiratory rate (bpm):',f[max_index]*60)
 
+  plt.plot(f[:N // 2], np.abs(fft_data)[:N // 2] * 1 / N)
   plt.xlabel('Frequency in Hertz [Hz]')
   plt.ylabel('Magnitude')
   plt.title(title)
@@ -127,9 +175,12 @@ def apply_fft(data, title):
 if __name__ == '__main__':
   data = np.loadtxt(input_file_path)
   print("Number of records:", len(data))
-
   plot_ax(data, 'Raw Accelerometer Data')
+
   data = segment(data)
   data = preprocess(data)
   plot_ax(data, 'Processed Accelerometer Data')
-  apply_fft_on_xyz(data)
+
+  print('Converting time domain signal to frequency domain by FFT...')
+  # r_x, r_y, r_z = apply_fft_on_xyz(data)
+  apply_kalman_filter(data)
