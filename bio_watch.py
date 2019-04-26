0ad5695c83d35bc8cfe3c387320987bb227d0de3
apoorvam <app.apoorva@gmail.com>
2019-04-26 17:33:27 -0400

    Use single fft function for calc of hr and br

--- bio_watch.py
+++ bio_watch.py
@@ -18,15 +18,13 @@ average_filter_window_duration_br = int((40/60)*sampling_frequency)
 T = 1/sampling_frequency
 
 def normalize(data):
-	data[:,0] = sp.stats.zscore(data[:,0])
-	data[:,1] = sp.stats.zscore(data[:,1])
-	data[:,2] = sp.stats.zscore(data[:,2])
-	return data
+  for i in range(0, 3):
+    data[:,i] = sp.stats.zscore(data[:,i])  
+  return data
 
 def apply_average_filter(data, window):
-  data[:,0] = np.array(pd.Series(data[:,0]).rolling(window=window).mean())
-  data[:,1] = np.array(pd.Series(data[:,1]).rolling(window=window).mean())
-  data[:,2] = np.array(pd.Series(data[:,2]).rolling(window=window).mean())
+  for i in range(0, 3):
+    data[:,i] = np.array(pd.Series(data[:,i]).rolling(window=window).mean())
   return data
 
 def plot(data, title):
@@ -61,7 +59,7 @@ def apply_bandpass_butterworth_filter(data, low_cutoff_freq, high_cutoff_freq):
 def aggregate_components(data):
   return np.array(list(map(lambda c: np.sqrt(np.sum(np.square(c))) , data)), dtype=np.float64)
 
-def fft_heart_rate(acc_data):
+def fft(acc_data, f_low, f_high):
   acc_data = acc_data[~np.isnan(acc_data)]
   acc_data = sp.signal.detrend(acc_data)
   N = len(acc_data)
@@ -81,45 +79,7 @@ def fft_heart_rate(acc_data):
   max_index = 0
   index = 0
   for c in fft_data:
-    if f[index] < 0.66 or f[index] > 2.5:
-      index = index + 1
-      continue;
-    real = np.real(c)
-    img = np.imag(c)
-    amp = np.sqrt(real*real + img*img)
-    if max_amp < amp:
-      max_amp = amp
-      max_index = index
-    index = index + 1
-
-  print('Max Amplitude:', max_amp)
-  print('Max Frequency:',f[max_index])
-  print('Heart Rate (bpm):', 60*f[max_index])
-
-def fft_breathing_rate(acc_data):
-  acc_data = acc_data[~np.isnan(acc_data)]
-  acc_data = sp.signal.detrend(acc_data)
-  N = len(acc_data)
-  T = 1/sampling_frequency
-  t = np.linspace(0, N/sampling_frequency, N)
-
-  fft_data = sp.fftpack.fft(acc_data)
-  T = t[1] - t[0]
-  f = np.linspace(0, 1/T, N)
-
-  plt.plot(f[:N // 2], np.abs(fft_data)[:N // 2] * 1 / N)
-  plt.xlabel('Frequency in Hertz [Hz]')
-  plt.ylabel('Magnitude')
-  plt.title('FFT')
-  plt.draw()
-  plt.pause(5)
-  plt.close()
-
-  max_amp = 0
-  index = 0
-  max_index = 0
-  for c in fft_data:
-    if f[index] < 0.13 or f[index] > 0.66:
+    if f[index] < f_low or f[index] > f_high:
       index = index + 1
       continue;
     real = np.real(c)
@@ -137,14 +97,16 @@ def calculate_breathing_rate(normalized_data):
   plot(smooth_data[:,0], 'Smoothened Accelerometer Data')
 
   amp_to_freq = {}
+  breathing_low_freq = 0.13
+  breathing_high_freq = 0.66
   print('Max Amplitude within 0.13 and 0.66 Hz frequency:')
-  br_x_amp, br_x_f = fft_breathing_rate(smooth_data[:,0])
+  br_x_amp, br_x_f = fft(smooth_data[:,0], breathing_low_freq, breathing_high_freq)
   amp_to_freq[br_x_amp] = br_x_f
   print('X-Axis:', br_x_amp)
-  br_y_amp, br_y_f = fft_breathing_rate(smooth_data[:,1])
+  br_y_amp, br_y_f = fft(smooth_data[:,1], breathing_low_freq, breathing_high_freq)
   amp_to_freq[br_y_amp] = br_y_f
   print('Y-Axis:', br_y_amp)
-  br_z_amp, br_z_f = fft_breathing_rate(smooth_data[:,2])
+  br_z_amp, br_z_f = fft(smooth_data[:,2], breathing_low_freq, breathing_high_freq)
   amp_to_freq[br_z_amp] = br_z_f
   print('Z-Axis:', br_z_amp)
   br_max_amp = max([br_x_amp, br_y_amp, br_z_amp])
@@ -156,7 +118,6 @@ def calculate_breathing_rate(normalized_data):
 def calculate_heart_rate(normalized_data):
   smooth_data = apply_average_filter(normalized_data, average_filter_window_duration_hr)
   smooth_data = np.array(list(filter(lambda row: np.isfinite(np.sum(row)), smooth_data)), dtype=np.float64)
-
   plot(smooth_data[:,0], 'Smoothened Accelerometer Data')
 
   high_cutoff_freq = 4
@@ -171,7 +132,10 @@ def calculate_heart_rate(normalized_data):
   bandpass2_data = apply_bandpass_butterworth_filter(aggregated_data, low_cutoff_freq, high_cutoff_freq)
   plot(bandpass2_data, 'Bandpass-2 Accelerometer Data')
 
-  fft_heart_rate(bandpass2_data)
+  max_amp, max_freq = fft(bandpass2_data, 0.66, 2.5)
+  print('Max Amplitude:', max_amp)
+  print('Max Frequency:', max_freq)
+  print('Heart Rate (bpm):', 60*max_freq)
   return
 
 if __name__ == '__main__':
